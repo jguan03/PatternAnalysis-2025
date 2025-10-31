ad97c7a978b0481fbe45872f588213dd7af183ea
jguan03 <jiaming.guan@student.uq.edu.au>
2025-10-31 22:13:58 +1000

    Define the UNet3D class structure and implemented encoder (downsampling path) which contains all of downsampling layers. Each layer progressively doubles the number of channels and extracts more and more abstract low-resolution features.

--- recognition/Prostate3D_47484333/module.py
+++ recognition/Prostate3D_47484333/module.py
@@ -34,4 +34,37 @@ class ConvBlock3D(nn.Module):
         # Pass the input tensor through the sequential layers
         return self.double_conv(x)
 
+    # --- 3D UNet ---
+
+class UNet3D(nn.Module):
+    """
+    A simple 3D UNet architecture for volume segmentation.
+    (Normal Difficulty Start)
+    """
+    def __init__(self, in_channels=1, out_classes=6, base_channels=32):
+
+        super(UNet3D, self).__init__()
+
+        # Encoder (Downsampling path)
+
+        # Level 1: Initial feature extraction. 
+        self.down1 = ConvBlock3D(in_channels, base_channels)
+        self.pool1 = nn.MaxPool3d(kernel_size=2, stride=2)
+
+        # Level 2: Deeper layer, double the channel to increase 
+        # the models capacity to represent and extract complex 
+        # when spartial resolution decreases. 
+        self.down2 = ConvBlock3D(base_channels, base_channels * 2)
+        self.pool2 = nn.MaxPool3d(kernel_size=2, stride=2)
+
+        # Level 3: Even deeper level, double base channels in the 
+        # convolution blocks. 
+        self.down3 = ConvBlock3D(base_channels * 2, base_channels * 4)
+        self.pool3 = nn.MaxPool3d(kernel_size=2, stride=2)
+
+        # Deepest layer - bottleneck layer which captures the most abstract, 
+        # low resolution information. 
+        self.bottleneck = ConvBlock3D(base_channels * 4, base_channels * 8)
+
+
 
