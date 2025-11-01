a736e57f2921538dd71d80c11e5ae8a12537d303
jguan03 <jiaming.guan@student.uq.edu.au>
2025-11-01 12:51:00 +1000

    Added a 3D dice score function which ccalculates the per class metrics from the output logits. Processes of this function include: converting raw input data to discrete class predictions, flattening 3D dimensions into a single vector, iterating through all the 5 different classes, as well as calculating the itersection and union between predicted and ground truths.

--- recognition/Prostate3D_47484333/train.py
+++ recognition/Prostate3D_47484333/train.py
@@ -12,12 +12,46 @@ import random
 from module import UNet3D
 from dataset import NIFTI3DSegmentationDataset, BATCH_SIZE, NUM_CLASSES, DEVICE, LR
 
-# Define the file path for the best model weights
+# Define the file path for the best model weights.
 MODEL_PATH = 'best_unet3d_model.pth'
 
-# Define the prostate label index globally
+# Define the prostate label index globally.
 PROSTATE_LABEL_IDX = 5
 
+def dice_score_3d(prediction, target, smooth=1e-6):
+    
+    # If the input is raw model output convert it to discrete class predictions.
+    if prediction.ndim == 5:
+
+        # (N, C, D, H, W) to (N, D, H, W)
+        prediction = torch.argmax(prediction, dim=1) 
+
+    # Flatten the 3D dimensions (D, H, W) into a single vector for easier per-voxel calculation.
+    prediction = prediction.contiguous().view(-1)
+    target = target.contiguous().view(-1)
+
+    # Store the calculated Dice score for each class.
+    dice_scores = []
+
+    # Loop through all of the different classes: 
+    for c in range(NUM_CLASSES):
+        # Create simple boolean masks for the current class.
+        pred_c = (prediction == c)
+        target_c = (target == c)
+
+        # Calculate True Positives (TPs) which is the overlap (intersection).
+        intersection = (pred_c * target_c).sum().float()
+        
+        # Calculate the sum of the predicted and ground truth volumes (union). 
+        union = pred_c.sum().float() + target_c.sum().float() 
+
+        # Dice formula: 2 * Intersection / (Total predicted + Total actual). 
+        dice = (2. * intersection + smooth) / (union + smooth)
+        dice_scores.append(dice.item())
+
+    # Return a list of dice scores. 
+    return dice_scores 
+    
 def train_unet_3d(model, train_loader, val_loader, epochs=50):
     return False
 
